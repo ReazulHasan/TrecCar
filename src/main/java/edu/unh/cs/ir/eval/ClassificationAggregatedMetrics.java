520b8dbc9d4de9cd6781754ebde7a4fb7b7177d0
Matt Magnusson <magnusson3@gmail.com>
2017-02-07 16:37:18 -0500

    Averaged performance measures added

---
+++ src/main/java/edu/unh/cs/ir/eval/ClassificationAggregatedMetrics.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+
+/**
+ * Created by matt on 2/7/17.
+ */
+public class ClassificationAggregatedMetrics {
+
+    private ArrayList<ClassificationBaseMetrics> results;
+
+    public ClassificationAggregatedMetrics(){
+        results=new ArrayList<ClassificationBaseMetrics>();
+    }
+
+    public ClassificationAggregatedMetrics(ArrayList<ClassificationBaseMetrics> metrics){
+        this.results=metrics;
+    }
+
+    public void addMetrics(ClassificationBaseMetrics c){
+        results.add(c);
+    }
+
+    public ArrayList<ClassificationBaseMetrics> getBaseMetrics(){
+        return results;
+    }
+
+    //Get precision for each complete result set averaged across all result sets
+    public double getPrecision(){
+        double accum =0;
+        for(ClassificationBaseMetrics m: results){
+            accum += m.getPrecision();
+        }
+        return accum/ results.size();
+    }
+
+    //Get precision at k averaged across all result sets
+    public double getPrecision(int k){
+        double accum =0;
+        for(ClassificationBaseMetrics m: results){
+            accum += m.getPrecision(k);
+        }
+        return accum/ results.size();
+    }
+
+    //Get R precision at k averaged across all result sets
+    public double getRPrecision(int k){
+        double accum =0;
+        for(ClassificationBaseMetrics m: results){
+            accum += m.getRPrecision(k);
+        }
+        return accum/ results.size();
+    }
+
+    //Get mean average precision across all result sets
+    public double getMAP(){
+        double accum =0;
+        for(ClassificationBaseMetrics m: results){
+            accum += m.getAveragePrecision();
+        }
+        return accum/ results.size();
+    }
+}
---
+++ src/test/java/edu/unh/cs/ir/eval/ClassificationAggregatedMetricsTest.java
@@ -0,0 +1,169 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by matt on 2/7/17.
+ */
+class ClassificationAggregatedMetricsTest {
+    int[] results1;
+    int[] results2;
+    int[] results3;
+
+    private ArrayList<Relevancy> resultsRelevancy1;
+    private ArrayList<Relevancy> resultsRelevancy2;
+    private ArrayList<Relevancy> resultsRelevancy3;
+
+    private ClassificationBaseMetrics eval1;
+    private ClassificationBaseMetrics eval2;
+    private ClassificationBaseMetrics eval3;
+
+    ClassificationAggregatedMetrics evals;
+
+    @BeforeEach
+    void setUp() {
+
+        results1 = new int[]{0,1,1,0,0,0,1,1,0,0};
+        resultsRelevancy1= new ArrayList<Relevancy>();
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT); //0
+        resultsRelevancy1.add(Relevancy.RELEVANT);    //1
+        resultsRelevancy1.add(Relevancy.RELEVANT);     //2
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT);  //3
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT);  //4
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT);  //5
+        resultsRelevancy1.add(Relevancy.RELEVANT);      //6
+        resultsRelevancy1.add(Relevancy.RELEVANT);      //7
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT); //8
+        resultsRelevancy1.add(Relevancy.NOT_RELEVANT); //9
+
+        results2 = new int[]{1,1,1,0,1,0,1,0,0,0};
+        resultsRelevancy2= new ArrayList<Relevancy>();
+        resultsRelevancy2.add(Relevancy.RELEVANT); //0
+        resultsRelevancy2.add(Relevancy.RELEVANT);    //1
+        resultsRelevancy2.add(Relevancy.RELEVANT);     //2
+        resultsRelevancy2.add(Relevancy.NOT_RELEVANT);  //3
+        resultsRelevancy2.add(Relevancy.RELEVANT);  //4
+        resultsRelevancy2.add(Relevancy.NOT_RELEVANT);  //5
+        resultsRelevancy2.add(Relevancy.RELEVANT);      //6
+        resultsRelevancy2.add(Relevancy.NOT_RELEVANT);      //7
+        resultsRelevancy2.add(Relevancy.NOT_RELEVANT); //8
+        resultsRelevancy2.add(Relevancy.NOT_RELEVANT); //9
+
+        results3 = new int[]{1,0,1,0,1,1,1,0,0,1};
+        resultsRelevancy3= new ArrayList<Relevancy>();
+        resultsRelevancy3.add(Relevancy.RELEVANT); //0
+        resultsRelevancy3.add(Relevancy.NOT_RELEVANT);    //1
+        resultsRelevancy3.add(Relevancy.RELEVANT);     //2
+        resultsRelevancy3.add(Relevancy.NOT_RELEVANT);  //3
+        resultsRelevancy3.add(Relevancy.RELEVANT);  //4
+        resultsRelevancy3.add(Relevancy.RELEVANT);  //5
+        resultsRelevancy3.add(Relevancy.RELEVANT);      //6
+        resultsRelevancy3.add(Relevancy.NOT_RELEVANT);      //7
+        resultsRelevancy3.add(Relevancy.NOT_RELEVANT); //8
+        resultsRelevancy3.add(Relevancy.RELEVANT); //9
+
+        eval1 = new ClassificationBaseMetrics(results1);
+        eval2 = new ClassificationBaseMetrics(results2);
+        eval3 = new ClassificationBaseMetrics(results3);
+
+        ArrayList<ClassificationBaseMetrics> evalList = new ArrayList<>();
+        evalList.add(eval1);
+        evalList.add(eval2);
+        evalList.add(eval3);
+
+         evals= new ClassificationAggregatedMetrics(evalList);
+    }
+
+    @Test
+    void testBaseMetrics() {
+        ArrayList<ClassificationBaseMetrics> evalList = evals.getBaseMetrics();
+
+        assertEquals(resultsRelevancy1,evalList.get(0).getResults());
+        assertEquals(resultsRelevancy2,evalList.get(1).getResults());
+        assertEquals(resultsRelevancy3,evalList.get(2).getResults());
+    }
+
+    @Test
+    void getPrecision(){
+        double p1 = 4.0/10;
+        double p2 = 5.0/10;
+        double p3 = 6.0/10;
+
+        double precision = (p1+p2+p3)/3;
+
+        assertEquals(precision,evals.getPrecision());
+    }
+    @Test
+    void getPrecisionAtK(){
+        int k=5;
+
+        double p1 = 2.0/k;
+        double p2 = 4.0/k;
+        double p3 = 3.0/k;
+
+        double precision = (p1+p2+p3)/3;
+
+        assertEquals(precision,evals.getPrecision(k));
+    }
+
+    @Test
+    void getRPrecision(){
+        int k=5;
+
+        double p1 = 2.0/4.0;
+        double p2 = 4.0/5.0;
+        double p3 = 3.0/6.0;
+
+        double rPrecision = (p1+p2+p3)/3;
+
+        assertEquals(rPrecision,evals.getRPrecision(k));
+    }
+
+    @Test
+    void getRPrecision2(){
+        int k=6;
+
+        int relevant1=20;
+        int relevant2=30;
+        int relevant3=100;
+
+        double p1 = 2.0/relevant1;
+        double p2 = 4.0/relevant2;
+        double p3 = 4.0/relevant3;
+
+        double rPrecision = (p1+p2+p3)/3;
+
+        eval1 = new ClassificationBaseMetrics(results1,relevant1);
+        eval2 = new ClassificationBaseMetrics(results2, relevant2);
+        eval3 = new ClassificationBaseMetrics(results3, relevant3);
+
+        evals= new ClassificationAggregatedMetrics();
+        evals.addMetrics(eval1);
+        evals.addMetrics(eval2);
+        evals.addMetrics(eval3);
+
+        assertEquals(rPrecision,evals.getRPrecision(k));
+    }
+
+    //Worked example from https://www.youtube.com/watch?v=pM6DJ0ZZee0
+    @Test
+    void getMAP(){
+        int[] results1 = {1,0,1,0,0,1,0,0,1,1};
+        int[] results2 = {0,1,0,0,1,0,1,0,0,0};
+
+        double map = 0.53;
+
+        eval1 = new ClassificationBaseMetrics(results1);
+        eval2 = new ClassificationBaseMetrics(results2);
+
+        evals= new ClassificationAggregatedMetrics();
+        evals.addMetrics(eval1);
+        evals.addMetrics(eval2);
+
+        assertEquals(map,evals.getMAP(),0.01);
+    }
+
+}
